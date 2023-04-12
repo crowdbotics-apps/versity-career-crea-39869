134004f86615e0186745fd473f7e290a8ce25e19
Crowdbotics <team@crowdbotics.com>
2023-04-12 19:01:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "versity-career-crea-39869",
+  url: "https://versity-career-crea-39869.botics.co",
+  api: "https://versity-career-crea-39869.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
